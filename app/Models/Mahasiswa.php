6a8cd64bf536a4e28e6feca272ce0ef8599db209
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-03-24 10:16:00 +0700

    Mensetting database yang digunakan pada file Model

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $table="mahasiswa"; 
+    public $timestamps= false;
+    protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_hp'
+    ];
+
 }
