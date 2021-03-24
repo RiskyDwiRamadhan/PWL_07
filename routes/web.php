2b35eb976761c74a4292892fb9f00d722154da1f
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-03-24 11:55:26 +0700

    Pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::resource('mahasiswa', MahasiswaController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
