c77a1c8400fa4d2837920cbf43b13e9f78046d6c
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-04-07 21:22:26 +0700

    Membuat relasi many to many antara table mahasiswa dan matkul

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -30,4 +30,9 @@ class Mahasiswa extends Model
     public function kelas(){
         return $this->belongsTo(Kelas::class);
     }
+    
+    public function matakuliah()
+    {
+        return $this->belongsToMany(MataKuliah::class, 'mahasiswa_matakuliah', 'mahasiswa_nim', 'matakuliah_id')->withPivot('nilai');
+    }
 }
